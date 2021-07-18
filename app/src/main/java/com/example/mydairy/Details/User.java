f452fa48920c42e2d263b15529052f55e1237128
SarthakT7 <sarthakthakkar752@gmail.com>
2021-07-18 18:30:44 +0530

    Database initiated

---
+++ app/src/main/java/com/example/mydairy/Details/User.java
@@ -0,0 +1,29 @@
+package com.example.mydairy.Details;
+
+public class User {
+
+    private  String name , surname , location;
+
+    public User()
+    {
+
+    }
+
+    public User(String name, String surname, String location) {
+        this.name = name;
+        this.surname = surname;
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+}
---
+++ app/src/main/java/com/example/mydairy/Details/UserDetails.java
@@ -0,0 +1,78 @@
+package com.example.mydairy.Details;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.mydairy.MainActivity;
+import com.example.mydairy.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class UserDetails extends AppCompatActivity {
+
+
+    private EditText editText ,editText2,editText3;
+    private Button button;
+    private  User user;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_details);
+
+        editText  = findViewById(R.id.editTextTextPersonName);
+        editText2  = findViewById(R.id.editTextTextPersonName2);
+        editText3  = findViewById(R.id.editTextTextPersonName3);
+        button= findViewById(R.id.button);
+
+
+        database = FirebaseDatabase.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String name = editText.getText().toString();
+                String surname = editText2.getText().toString();
+                String location = editText3 .getText().toString();
+                user = new User(name,surname,location);
+                updateUI();
+            }
+        });
+
+    }
+
+
+        public void updateUI() {
+            String keyid;
+            Bundle extras = getIntent().getExtras();
+            if(extras == null) {
+                keyid= null;
+            } else {
+                keyid= extras.getString("UID");
+            }
+            //String keyid = databaseReference.push().getKey();
+            databaseReference.child("users").child(keyid).setValue(user); //adding user info to database
+            System.out.println("jinil: " +keyid);
+            System.out.println("jinil2"+user.getLocation());
+
+
+    }
+
+
+}
