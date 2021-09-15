5c9c9991049f4738534274d6408dcd28649028f2
jinil3108 <19bce183@nirmauni.ac.in>
2021-09-15 21:53:58 +0530

    ગ્રાફ કરી નાખ્યા મોર્નિંગ અને ઈવેનઈંગ ના ફેટ અને ક્વોન્ટિટી માં

--- app/src/main/java/com/example/mydairy/specific/GraphFat.java
+++ app/src/main/java/com/example/mydairy/specific/GraphFat.java
@@ -30,12 +30,12 @@ import java.util.ArrayList;
 
 public class GraphFat extends AppCompatActivity {
 
-    private LineChart mChart;
+    private LineChart mChart,mChart2;
     private FirebaseDatabase database;
-    private DatabaseReference mPostReference;
-    ValueEventListener valueEventListener;
-    ArrayList<Entry> yData;
-    ArrayList<String> xData;
+    private DatabaseReference mPostReference,mPostReference2;
+    ValueEventListener valueEventListener,valueEventListener2;
+    ArrayList<Entry> yData1,yData2;
+    ArrayList<String> xData1,xData2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +43,8 @@ public class GraphFat extends AppCompatActivity {
 
         mChart = (LineChart)findViewById(R.id.specific_graph);
         mChart.animateX(1000);
+        mChart2 = (LineChart)findViewById(R.id.specific_graph1);
+        mChart2.animateX(1000);
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         String keyid = user.getUid();
         mPostReference = FirebaseDatabase.getInstance().getReference().child("users").child(keyid).child("DailyEntry");
@@ -51,22 +53,38 @@ public class GraphFat extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull @NotNull DataSnapshot dataSnapshot) {
 
-                    yData = new ArrayList<>();
-                    xData = new ArrayList<String>();
+                    yData1 = new ArrayList<>();
+                    yData2 = new ArrayList<>();
+                    xData1 = new ArrayList<String>();
+                    xData2 = new ArrayList<String>();
 
-                    float i =0;
+                    float i =0,j = 0;
                     for (DataSnapshot ds : dataSnapshot.getChildren()){
                         i=i+1;
 
-                        String SV = ds.child("evening").child("fat").getValue().toString();
-                        Float SensorValue = Float.parseFloat(SV);
+                        String SV1 = ds.child("morning").child("fat").getValue().toString();
+                        Float SensorValue1 = Float.parseFloat(SV1);
 
-                        yData.add(new Entry(i,SensorValue));
-                        xData.add(ds.getKey());
+                        yData1.add(new Entry(i,SensorValue1));
+                        xData1.add(ds.getKey());
+
+                    }
+                    i=0;
+                    for (DataSnapshot ds : dataSnapshot.getChildren()){
+                        i=i+1;
+
+                        String SV2 = ds.child("evening").child("fat").getValue().toString();
+                        Float SensorValue2 = Float.parseFloat(SV2);
+
+                        yData2.add(new Entry(i,SensorValue2));
+                        xData2.add(ds.getKey());
+                        Log.i("Jinil",ds.getKey());
 
                     }
 
-                    final LineDataSet lineDataSet = new LineDataSet(yData,"Fat");
+                    final LineDataSet lineDataSet;
+                    lineDataSet= new LineDataSet(yData1,"Morning Fat");
+                    lineDataSet.setColor(Color.BLUE);
                     LineData data = new LineData(lineDataSet);
                     XAxis xaxis =mChart.getXAxis();
                     xaxis.setPosition(XAxis.XAxisPosition.BOTTOM);
@@ -78,21 +96,51 @@ public class GraphFat extends AppCompatActivity {
                         @Override
                         public String getFormattedValue(float value, AxisBase axis) {
 
-                            return xData.get((int) value - 1);
+                            return xData1.get((int) value - 1);
                         }
                     });
 
-                    if (xData.isEmpty()||xData.size()==1) {
+                    if (xData1.isEmpty()||xData1.size()==1) {
                         mChart.clear();
                     } else {
                         // set data
                         mChart.setData(data);
-                        mChart.setBackgroundColor(Color.rgb(244, 117, 117));
+                        //mChart.setBackgroundColor(Color.rgb(244, 117, 117));
                     }
                     mChart.getXAxis().setGranularityEnabled(true);
-                    mChart.setNoDataText("Sorry! No Data Found");
+                    mChart.setNoDataText("Please add atleast 2 Entries");
                     mChart.notifyDataSetChanged();
                     mChart.invalidate();
+
+                final LineDataSet lineDataSet1;
+                lineDataSet1= new LineDataSet(yData2,"Evening Fat");
+                lineDataSet1.setColor(Color.BLACK);
+                LineData data1 = new LineData(lineDataSet1);
+                XAxis xaxis1 =mChart2.getXAxis();
+                xaxis1.setPosition(XAxis.XAxisPosition.BOTTOM);
+                xaxis1.setCenterAxisLabels(false);
+                YAxis yAxisRight1 = mChart2.getAxisRight();
+                yAxisRight1.setEnabled(false);
+                xaxis1.setDrawGridLines(true);
+                xaxis1.setValueFormatter(new IAxisValueFormatter() {
+                    @Override
+                    public String getFormattedValue(float value, AxisBase axis) {
+
+                        return xData2.get((int) value - 1);
+                    }
+                });
+
+                if (xData2.isEmpty()||xData2.size()==1) {
+                    mChart2.clear();
+                } else {
+                    // set data
+                    mChart2.setData(data1);
+                    //mChart.setBackgroundColor(Color.rgb(244, 117, 117));
+                }
+                mChart2.getXAxis().setGranularityEnabled(true);
+                mChart2.setNoDataText("Please add atleast 2 Entries");
+                mChart2.notifyDataSetChanged();
+                mChart2.invalidate();
 
             }
 
@@ -101,5 +149,8 @@ public class GraphFat extends AppCompatActivity {
                     Log.i("Graph Error: ",error.getMessage());
             }
         });
+
+
+
     }
 }
--- app/src/main/java/com/example/mydairy/specific/GraphQty.java
+++ app/src/main/java/com/example/mydairy/specific/GraphQty.java
@@ -30,12 +30,12 @@ import java.util.ArrayList;
 
 public class GraphQty extends AppCompatActivity {
 
-    private LineChart mChart;
+    private LineChart mChart,mChart2;
     private FirebaseDatabase database;
     private DatabaseReference mPostReference;
     ValueEventListener valueEventListener;
-    ArrayList<Entry> yData;
-    ArrayList<String> xData;
+    ArrayList<Entry> yData,yData1;
+    ArrayList<String> xData,xData1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +45,8 @@ public class GraphQty extends AppCompatActivity {
 
         mChart = (LineChart)findViewById(R.id.specific_graph);
         mChart.animateX(1000);
+        mChart2 = (LineChart)findViewById(R.id.specific_graph1);
+        mChart2.animateX(1000);
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         String keyid = user.getUid();
         mPostReference = FirebaseDatabase.getInstance().getReference().child("users").child(keyid).child("DailyEntry");
@@ -54,21 +56,37 @@ public class GraphQty extends AppCompatActivity {
             public void onDataChange(@NonNull @NotNull DataSnapshot dataSnapshot) {
 
                 yData = new ArrayList<>();
+                yData1 = new ArrayList<>();
                 xData = new ArrayList<String>();
+                xData1 = new ArrayList<String>();
+                Object []xD = xData.toArray();
+                Object []xD1 = xData1.toArray();
 
                 float i =0;
                 for (DataSnapshot ds : dataSnapshot.getChildren()){
                     i=i+1;
 
-                    String SV = ds.child("evening").child("quantity").getValue().toString();
+                    String SV = ds.child("morning").child("quantity").getValue().toString();
                     Float SensorValue = Float.parseFloat(SV);
 
                     yData.add(new Entry(i,SensorValue));
                     xData.add(ds.getKey());
 
                 }
+                i=0;
+                for (DataSnapshot ds : dataSnapshot.getChildren()){
+                    i=i+1;
 
-                final LineDataSet lineDataSet = new LineDataSet(yData,"Quantity");
+                    String SV2 = ds.child("evening").child("quantity").getValue().toString();
+                    Float SensorValue2 = Float.parseFloat(SV2);
+
+                    yData1.add(new Entry(i,SensorValue2));
+                    xData1.add(ds.getKey());
+                    Log.i("Jinil",ds.getKey());
+
+                }
+
+                final LineDataSet lineDataSet = new LineDataSet(yData,"Morning Quantity");
                 LineData data = new LineData(lineDataSet);
                 XAxis xaxis =mChart.getXAxis();
                 xaxis.setPosition(XAxis.XAxisPosition.BOTTOM);
@@ -76,11 +94,12 @@ public class GraphQty extends AppCompatActivity {
                 YAxis yAxisRight = mChart.getAxisRight();
                 yAxisRight.setEnabled(false);
                 xaxis.setDrawGridLines(true);
-                xaxis.setValueFormatter(new IAxisValueFormatter() {
+                xaxis.setValueFormatter(new IAxisValueFormatter()
+                {
                     @Override
                     public String getFormattedValue(float value, AxisBase axis) {
 
-                        return xData.get((int) value - 1);
+                        return xData.get((int)value-1);
                     }
                 });
 
@@ -89,12 +108,42 @@ public class GraphQty extends AppCompatActivity {
                 } else {
                     // set data
                     mChart.setData(data);
-                    mChart.setBackgroundColor(Color.rgb(244, 117, 117));
+                    //mChart.setBackgroundColor(Color.rgb(244, 117, 117));
                 }
                 mChart.getXAxis().setGranularityEnabled(true);
-                mChart.setNoDataText("Sorry! No Data Found");
+                mChart.setNoDataText("Please add atleast 2 Entries");
                 mChart.notifyDataSetChanged();
                 mChart.invalidate();
+
+                final LineDataSet lineDataSet1;
+                lineDataSet1= new LineDataSet(yData1,"Evening Quantity");
+                lineDataSet1.setColor(Color.BLACK);
+                LineData data1 = new LineData(lineDataSet1);
+                XAxis xaxis1 =mChart2.getXAxis();
+                xaxis1.setPosition(XAxis.XAxisPosition.BOTTOM);
+                xaxis1.setCenterAxisLabels(false);
+                YAxis yAxisRight1 = mChart2.getAxisRight();
+                yAxisRight1.setEnabled(false);
+                xaxis1.setDrawGridLines(true);
+                xaxis1.setValueFormatter(new IAxisValueFormatter() {
+                    @Override
+                    public String getFormattedValue(float value, AxisBase axis) {
+
+                        return xData1.get((int) value - 1);
+                    }
+                });
+
+                if (xData1.isEmpty()||xData1.size()==1) {
+                    mChart2.clear();
+                } else {
+                    // set data
+                    mChart2.setData(data1);
+                    //mChart.setBackgroundColor(Color.rgb(244, 117, 117));
+                }
+                mChart2.getXAxis().setGranularityEnabled(true);
+                mChart2.setNoDataText("Please add atleast 2 Entries");
+                mChart2.notifyDataSetChanged();
+                mChart2.invalidate();
 
             }
 
